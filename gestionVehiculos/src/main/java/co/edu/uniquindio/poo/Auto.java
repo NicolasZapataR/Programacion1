97cb982e4a1a791e66713c2e09affabbeb9e5320
NicolasZapataR <nicolas.zapatar@uqvirtual.edu.co>
2024-10-26 10:06:32 -0500

    update

---
+++ gestionVehiculos/src/main/java/co/edu/uniquindio/poo/Auto.java
@@ -0,0 +1,35 @@
+package co.edu.uniquindio.poo;
+
+public class Auto extends vehiculo {
+
+    public Auto(String placa, String marca, int modelo, String añoFabricacion) {
+        super(placa, marca, modelo, añoFabricacion);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public double calcularCostoRenta() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calcularCostoRenta'");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
